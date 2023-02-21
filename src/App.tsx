330ea80692a37fa515e2aa4d2f9890656a6ace44
Saleh-Alibrahim <s.albrahim@outlook.com>
2023-02-21 11:59:52 +0300

    finish if branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './app.css';
 const App = () => {
   const age = 10;
 
+  const hardSum = 5125215 + 123213;
   let res;
 
   if (age > 20) {
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div className='App'>
       <h1>{res}</h1>
+      <h1>{hardSum}</h1>
       <h1 className='mt-5'>
         {age > 20 ? 'You are older than 20' : 'You are less than 20'}
       </h1>
